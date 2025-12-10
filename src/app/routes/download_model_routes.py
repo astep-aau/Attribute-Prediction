16806ae31096e24e6d02814155a1ce3383540a6d
MikkelTroelsen <mikkel.s.troelsen@gmail.com>
2025-12-10 10:51:33 +0100

    download model scaffoldning

--- src/app/routes/download_model_routes.py
+++ src/app/routes/download_model_routes.py
@@ -1,13 +1,15 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from fastapi.responses import FileResponse
+import os
 
 router = APIRouter(prefix="/download_model", tags=["download model"])
 
 @router.get("/{model_id}", response_class=FileResponse)
 def download_model(model_id: str):
 
+    file_path = "testfile.txt"
     return FileResponse(
-        path='testfile.txt',
+        path=file_path,
         media_type="application/octet-stream",
         filename=f"{model_id}.txt"
-    )
+)
