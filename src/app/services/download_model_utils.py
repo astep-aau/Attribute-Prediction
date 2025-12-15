15ebbca47ee04dcc3681ca717d371b2ba0565baf
MikkelTroelsen <mikkel.s.troelsen@gmail.com>
2025-12-15 09:00:39 +0100

    download improvements

--- src/app/services/download_model_utils.py
+++ src/app/services/download_model_utils.py
@@ -2,6 +2,7 @@ from uuid import UUID
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 from fastapi.responses import FileResponse
+import os
 from src.app.database_tables import (
     ModelMetricsTable
 )
@@ -13,10 +14,13 @@ from src.app.exceptions import (
 async def build_file_response(model_id :str, db: AsyncSession):
     file_path = await get_model_path(model_id, db)
     file_name = get_file_name(file_path)
-    return FileResponse(
+
+    response = FileResponse(
         path=file_path,
         media_type="application/octet-stream",
-        filename=f"{file_name}.pth")
+        filename=file_name)
+
+    return response
 
 async def get_model_path(model_id: str, db: AsyncSession):
     try:
@@ -33,7 +37,11 @@ async def get_model_path(model_id: str, db: AsyncSession):
     if not path:
         raise NotFoundException(f"No path for model: {model_id}")
 
+    if not os.path.isfile(path):
+        raise NotFoundException(f"Model wfile not found at: {path}")
     return path
 
 def get_file_name(filepath: str):
-    return filepath.split("/")[-1]
+    base_name = filepath.split("/")[-1]
+    file_name = base_name.rsplit(".", 1)[0] + ".pth"
+    return file_name
